1721e5a3e9ae1d722cf8a13a353de3a2183341d4
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-12-15 16:10:51 -0300

    Fix next config for Vercel (ESM/CJS)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ✅ No uses output: "export" (rompe /api + Prisma)
+  // ✅ no uses output: "export"
   // output: "export",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
