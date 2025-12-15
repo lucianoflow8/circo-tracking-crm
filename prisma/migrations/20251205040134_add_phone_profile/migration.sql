68b75d64f6328f317cb2a579711f09bee5515406
lucianoflow8 <maximilianoluciano2006@gmail.com>
2025-12-14 23:09:12 -0300

    Reset prisma migrations (sqlite -> postgres)

--- prisma/migrations/20251205040134_add_phone_profile/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "PhoneProfile" (
-    "phone" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT,
-    "avatarUrl" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
